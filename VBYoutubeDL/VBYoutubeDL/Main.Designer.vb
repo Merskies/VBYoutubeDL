cda7ddc358006607d91ea82dd47aa8d6aa06bfa1
Merskies <32473694+Merskies@users.noreply.github.com>
2017-12-15 03:45:46 -0800

    VBYoutubeDL_v.1

---
+++ VBYoutubeDL/VBYoutubeDL/Main.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.txtURL = New System.Windows.Forms.TextBox()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.txtFolder = New System.Windows.Forms.TextBox()
+        Me.lblURL = New System.Windows.Forms.Label()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.lblFolder = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(159, 299)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(100, 65)
+        Me.btnRun.TabIndex = 0
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'txtURL
+        '
+        Me.txtURL.Location = New System.Drawing.Point(78, 273)
+        Me.txtURL.Name = "txtURL"
+        Me.txtURL.Size = New System.Drawing.Size(124, 20)
+        Me.txtURL.TabIndex = 1
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(12, 25)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(395, 220)
+        Me.txtOutput.TabIndex = 2
+        '
+        'txtFolder
+        '
+        Me.txtFolder.Location = New System.Drawing.Point(216, 273)
+        Me.txtFolder.Name = "txtFolder"
+        Me.txtFolder.Size = New System.Drawing.Size(129, 20)
+        Me.txtFolder.TabIndex = 3
+        '
+        'lblURL
+        '
+        Me.lblURL.AutoSize = True
+        Me.lblURL.Location = New System.Drawing.Point(125, 256)
+        Me.lblURL.Name = "lblURL"
+        Me.lblURL.Size = New System.Drawing.Size(29, 13)
+        Me.lblURL.TabIndex = 4
+        Me.lblURL.Text = "URL"
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Location = New System.Drawing.Point(190, 9)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(39, 13)
+        Me.lblOutput.TabIndex = 5
+        Me.lblOutput.Text = "Output"
+        '
+        'lblFolder
+        '
+        Me.lblFolder.AutoSize = True
+        Me.lblFolder.Location = New System.Drawing.Point(266, 256)
+        Me.lblFolder.Name = "lblFolder"
+        Me.lblFolder.Size = New System.Drawing.Size(36, 13)
+        Me.lblFolder.TabIndex = 6
+        Me.lblFolder.Text = "Folder"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(419, 376)
+        Me.Controls.Add(Me.lblFolder)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.lblURL)
+        Me.Controls.Add(Me.txtFolder)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.txtURL)
+        Me.Controls.Add(Me.btnRun)
+        Me.Name = "frmMain"
+        Me.Text = "YoutubeDL"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRun As Button
+    Friend WithEvents txtURL As TextBox
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents txtFolder As TextBox
+    Friend WithEvents lblURL As Label
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents lblFolder As Label
+End Class
---
+++ VBYoutubeDL/VBYoutubeDL/Main.vb
@@ -0,0 +1,46 @@
+﻿Public Class frmMain
+    Private psi As ProcessStartInfo
+    Private cmd As Process
+    Private Delegate Sub InvokeWithString(ByVal text As String)
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRun.Click
+        Dim Command, Folder, URL As String
+        Folder = txtFolder.Text
+        URL = txtURL.Text
+        Command = "youtube-dl.exe"
+        Dim Arguments As String = "-i -o \\MERSKIES\rootshare\Media\Music\Downloaded\" & Folder & "\%(title)s.%(ext)s " & URL & " -f bestaudio"
+        Try
+            cmd.Kill()
+        Catch ex As Exception
+        End Try
+        txtOutput.Clear()
+        If Arguments <> "" Then
+            psi = New ProcessStartInfo(Command, Arguments)
+        Else
+            psi = New ProcessStartInfo(Command)
+        End If
+        Dim systemencoding As System.Text.Encoding
+        System.Text.Encoding.GetEncoding(Globalization.CultureInfo.CurrentUICulture.TextInfo.OEMCodePage)
+        With psi
+            .UseShellExecute = False
+            .RedirectStandardError = True
+            .RedirectStandardOutput = True
+            .RedirectStandardInput = True
+            .CreateNoWindow = True
+            .StandardOutputEncoding = systemencoding
+            .StandardErrorEncoding = systemencoding
+        End With
+        cmd = New Process With {.StartInfo = psi, .EnableRaisingEvents = True}
+        AddHandler cmd.ErrorDataReceived, AddressOf Async_Data_Received
+        AddHandler cmd.OutputDataReceived, AddressOf Async_Data_Received
+        cmd.Start()
+        cmd.BeginOutputReadLine()
+        cmd.BeginErrorReadLine()
+    End Sub
+    Private Sub Async_Data_Received(ByVal sender As Object, ByVal e As DataReceivedEventArgs)
+        Me.Invoke(New InvokeWithString(AddressOf Sync_Output), e.Data)
+    End Sub
+    Private Sub Sync_Output(ByVal text As String)
+        txtOutput.AppendText(text & Environment.NewLine)
+        txtOutput.ScrollToCaret()
+    End Sub
+End Class
